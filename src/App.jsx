cf6934b11a0d3f51a9771e201ed3c10d7563a193
Daniel-Vergel <danielvergel33@gmail.com>
2024-05-07 10:55:13 -0500

    Frame 1014 completed

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+import { Frame1014 } from './frame1014'
+
+
+function App() {
+
+
+  return (
+    <>
+      <nav className=' flex w-full bg-white h-72 border-b shadow-lg border-cyan-500 '>
+
+          <div className='flex items-center'>
+                  <img 
+                          src="https://cs3.com.co/uploads/2/2019-04/logo_cs3_2.png" 
+                          alt="logo cs3"
+                          className=' w-32 h-14 ml-16 mr-2'
+                  />
+                    <div className='font-semibold text-xs text-cyan-800'>
+                        <div>
+                          COMERCIALIZADORA DE SOFTWARE,
+                        </div>
+                        <div>
+                          SERVICIOS T SUMINISTROS S.A.S.
+                        </div>
+                    </div>
+           </div>
+      </nav>
+      <Frame1014/>  
+      <nav className=' fixed flex w-204 h-full bg-slate-900'>
+        <div>
+
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default App
